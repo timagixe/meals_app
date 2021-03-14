5ad1087e4dd038ee87047c046b7823bb5d22a176
Tima Gixe <timagixe@protonmail.com>
2021-03-14 18:13:17 +0200

    feat(screens): :sparkles: create categories screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/screens/categories.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,35 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: 'Meals'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({
-    Key key,
-    this.title,
-  }) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text(
-          'Meals Project',
-        ),
-      ),
+      home: CategoriesScreen(),
     );
   }
 }
---
+++ lib/screens/categories.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/widgets/category_item.dart';
+
+import '../constants/categories.dart';
+
+const double _kmaxCrossAxisExtent = 200.0;
+const double _kchildAspectRatio = 3 / 2;
+const double _kcrossAxisSpacing = 20.0;
+const double _kmainAxisSpacing = 20.0;
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    List<CategoryItem> categoryItems = CATEGORIES
+        .map((data) => CategoryItem(
+              title: data.title,
+              color: data.color,
+            ))
+        .toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meals'),
+      ),
+      body: GridView(
+        children: categoryItems,
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: _kmaxCrossAxisExtent,
+          childAspectRatio: _kchildAspectRatio,
+          crossAxisSpacing: _kcrossAxisSpacing,
+          mainAxisSpacing: _kmainAxisSpacing,
+        ),
+      ),
+    );
+  }
+}
