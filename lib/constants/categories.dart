2f65c85c74803f40f62ccda44133cac74ab31631
Tima Gixe <timagixe@protonmail.com>
2021-03-14 18:11:25 +0200

    feat(mocks): :clown_face: create categories mock data

---
+++ lib/constants/categories.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../models/category.dart';
+
+const CATEGORIES = const [
+  Category(
+    id: '2a2d07a6-fa48-424a-b666-7a2575aaefa1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: '3dd47015-b3ea-4c62-8e5d-7d74f0063846',
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  Category(
+    id: '5235d750-0a9a-4852-a4cd-fd6b3bd31cce',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Category(
+    id: '359c3958-e0f7-4f62-b4c1-b31f18737c6a',
+    title: 'German',
+    color: Colors.amber,
+  ),
+  Category(
+    id: '0e34b613-d329-42b7-bc87-dfa87008783b',
+    title: 'Light & Lovely',
+    color: Colors.blue,
+  ),
+  Category(
+    id: '047903d4-d2e7-4ca1-87c7-5d36f1c39cc1',
+    title: 'Exotic',
+    color: Colors.green,
+  ),
+  Category(
+    id: '752779c6-14f9-4004-afc5-ca15833b69a3',
+    title: 'Breakfast',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: '5ef23ba0-bda9-4900-ba5a-5b685275efae',
+    title: 'Asian',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: '168ad065-7ba7-47c6-814f-b59e1e5be987',
+    title: 'French',
+    color: Colors.pink,
+  ),
+  Category(
+    id: '5a20a4a2-8d4c-46df-b950-a7ab549288ca',
+    title: 'Summer',
+    color: Colors.teal,
+  ),
+];
