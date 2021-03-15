82f7fafd7c7f87331b6da2d1998dbb5d379d391e
Tima Gixe <timagixe@protonmail.com>
2021-03-15 07:31:32 +0200

    feat(routes): :sparkles: update list of routes

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/screens/recipe_details.dart';
 
 import '../screens/categories.dart';
 import '../screens/category_recipes.dart';
@@ -6,6 +7,7 @@ import '../screens/category_recipes.dart';
 class AppRoutes {
   static const String ROOT = '/';
   static const String CATEGORY_RECIPES = '/category-recipes';
+  static const String RECIPE_DETAILS = '/recipe-details';
 }
 
 const String kAppInitialRoute = AppRoutes.ROOT;
@@ -13,4 +15,5 @@ const String kAppInitialRoute = AppRoutes.ROOT;
 Map<String, WidgetBuilder> appRoutes = {
   AppRoutes.ROOT: (context) => CategoriesScreen(),
   AppRoutes.CATEGORY_RECIPES: (context) => CategoryRecipesScreen(),
+  AppRoutes.RECIPE_DETAILS: (context) => RecipeDetailsScreen(),
 };
