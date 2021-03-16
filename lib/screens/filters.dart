aa8c8b6d0f6222e90974a7b66b37edd330326546
Tima Gixe <timagixe@protonmail.com>
2021-03-16 10:51:03 +0200

    feat(screens): :sparkles: add switch tiles to filters screen

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -2,7 +2,31 @@ import 'package:flutter/material.dart';
 
 import '../widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFree = false;
+  var _vegetarian = false;
+  var _vegan = false;
+  var _lactoseFree = false;
+
+  Widget _buildSwitchListTile({
+    @required String title,
+    @required String subtitle,
+    @required bool currentValue,
+    @required Function onSwitch,
+  }) {
+    return SwitchListTile(
+      title: Text(title),
+      value: currentValue,
+      subtitle: Text(subtitle),
+      onChanged: onSwitch,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,8 +34,57 @@ class FiltersScreen extends StatelessWidget {
         title: Text('Your Filters'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Filters Screen'),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              'Adjust your recipes section',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          ),
+          Expanded(
+              child: ListView(
+            children: [
+              _buildSwitchListTile(
+                  title: 'Gluten free',
+                  subtitle: 'Only include gluten free recipes',
+                  currentValue: _glutenFree,
+                  onSwitch: (newValue) {
+                    setState(() {
+                      _glutenFree = newValue;
+                    });
+                  }),
+              _buildSwitchListTile(
+                  title: 'Vegetarian',
+                  subtitle: 'Only include vegetarian recipes',
+                  currentValue: _vegetarian,
+                  onSwitch: (newValue) {
+                    setState(() {
+                      _vegetarian = newValue;
+                    });
+                  }),
+              _buildSwitchListTile(
+                  title: 'Vegan',
+                  subtitle: 'Only include vegan recipes',
+                  currentValue: _vegan,
+                  onSwitch: (newValue) {
+                    setState(() {
+                      _vegan = newValue;
+                    });
+                  }),
+              _buildSwitchListTile(
+                  title: 'Lactose free',
+                  subtitle: 'Only include lactose free recipes',
+                  currentValue: _lactoseFree,
+                  onSwitch: (newValue) {
+                    setState(() {
+                      _lactoseFree = newValue;
+                    });
+                  }),
+            ],
+          ))
+        ],
       ),
     );
   }
